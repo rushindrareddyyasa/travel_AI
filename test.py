c69a6bf05e846fbfb3fe34d743bcf4788e83462e
rushindrareddyyasa <y.rushindrareddy@gmail.com>
2026-09-13 19:02:01 +0530

    App build completed

--- test.py
+++ test.py
@@ -1,8 +1,19 @@
 from tools.tavily_tool import tavily_search
 from tools.flight_tool import search_flights
+from backend import run_travel_agent
 
 # res = tavily_search("best hotels in hyderabad")
 # print(res)
 
-res = search_flights("Give me a plan of 7 days trip from hyd to banglore")
-print(res)
+# res = search_flights("Give me a plan of 7 days trip from hyd to banglore")
+# print(res)
+
+
+user_input = input("Enter Travel request:")
+response = run_travel_agent(
+    user_input=user_input,
+    thread_id="test_user"
+)
+
+print("\nFINAL_RESPONSE:\n")
+print(response["answer"])
